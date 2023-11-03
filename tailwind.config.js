b6866474b52dfffb69e5a5c4be7e4ff210cf23c5
Pedro <128544268+Pedro-Reiser-da-Costa@users.noreply.github.com>
2023-11-03 18:01:38 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        'colorido': '#E57BA1' ,
+      },
+
+      fontFamily: {
+        'montserrat': ['Montserrat'],
+    },
+    },
+  },
+  plugins: [],
+}
+
